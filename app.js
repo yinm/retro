c7a61f08ba24b072f6281ed08a5f226c3c1c9e05
yinm <yinm.cminor9@gmail.com>
2017-08-16 12:18:12 +0900

    get titles of issue and PR events

--- app.js
+++ app.js
@@ -19,17 +19,31 @@ https.get(option, (res) => {
 
   res.on('end', () => {
     const data = JSON.parse(body);
-    const ids = getIds(data);
-    console.log(ids);
+    const wantedEventsData = getWantedEventsData(data);
+    console.log(wantedEventsData);
   });
 });
 
-function getIds(data) {
-  let ids = '';
+function getWantedEventsData(data) {
+  let wantedEventsData = '';
 
   for (let i = 0, length = data.length; i < length; i++) {
-    ids += data[i].id + '\n';
+    switch (data[i].type) {
+      case 'IssuesEvent':
+      case 'IssueCommentEvent':
+        wantedEventsData += data[i].payload.issue.title + '\n';
+        break;
+
+      case 'PullRequestEvent':
+      case 'PullRequestReviewCommentEvent':
+        wantedEventsData += data[i].payload.pull_request.title + '\n';
+        break;
+
+      default:
+        wantedEventsData += `${i}には、なかったよ\n`; // for debug
+        break;
+    }
   }
 
-  return ids;
+  return wantedEventsData;
 }
